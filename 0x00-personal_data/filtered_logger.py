0cd07b133d6aef325517c985a9d41b0df20964f2
mahakamal-e <mahakamal09@gmail.com>
2024-08-02 05:01:39 +0000

    edit

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
 import re
 from typing import List
 
+
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """Returns the log message obfuscated"""
