85861b98874b39708b3d57008f5a7ef71f57d6d1
mahakamal-e <mahakamal09@gmail.com>
2024-08-06 23:30:33 +0000

    task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,7 +11,17 @@ class Auth:
     """Implement class Auth """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """returns False - path and excluded_paths """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        
+        path = path.rstrip('/')
+        for excluded_path in excluded_paths:
+            excluded_path = excluded_path.rstrip('/')
+            if path == excluded_path or path.startswith(excluded_path + '/'):
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """returns None - request"""
