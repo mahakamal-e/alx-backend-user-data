90e9833e6b1266c7267a7455c5d841bdc793e759
mahakamal-e <mahakamal09@gmail.com>
2024-08-07 01:36:27 +0000

    fix error

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -20,14 +20,13 @@ class Auth:
         for excluded_path in excluded_paths:
             excluded_path = excluded_path.rstrip('/')
             if excluded_path.endswith('*'):
-            # Remove the '*' for prefix matching
-            excluded_prefix = excluded_path[:-1]
-            if path.startswith(excluded_prefix):
-                return False
+                excluded_prefix = excluded_path[:-1]
+                if path.startswith(excluded_prefix):
+                    return False
             else:
                 if (path == excluded_path or 
                     path.startswith(excluded_path + '/')):
-                return False
+                    return False
         return True
 
     def authorization_header(self, request=None) -> str:
