9c993cb4e8e0ee8174bd54392bfba891e810a579
mahakamal-e <mahakamal09@gmail.com>
2024-08-08 23:06:53 +0000

    edit

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -29,24 +29,17 @@ class SessionExpAuth(SessionAuth):
 
     def user_id_for_session_id(self, session_id=None):
         """Gets user_id of a specific session"""
-        if not session_id or session_id not in self.user_id_by_session_id:
+        if not session_id:
             return None
-
-        session_dict = self.user_id_by_session_id.get(session_id)
-        session_created_at = session_dict.get('created_at')
-
-        if not session_created_at:
+        session = self.user_id_by_session_id.get(session_id)
+        if not session:
             return None
-
-        if not self.is_session_expired(session_created_at):
-            return None
-
-        return session_dict.get('user_id')
-
-    def is_session_expired(self, created_at):
-        """Checks if a session is expired"""
         if self.session_duration <= 0:
-            return True
-
-        expiration_time = created_at + timedelta(seconds=self.session_duration)
-        return expiration_time >= datetime.now()
+            return session['user_id']
+        created_at = session.get('created_at')
+        if not created_at:
+            return None
+        if datetime.now() > created_at + timedelta(seconds=self.session_duration):
+            del self.user_id_by_session_id[session_id]
+            return None
+        return session['user_id']
