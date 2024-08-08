bc2faf7d0056b9e84f8f8c3ea95209ef404ae1e8
mahakamal-e <mahakamal09@gmail.com>
2024-08-08 22:25:41 +0000

    d

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -32,14 +32,13 @@ class SessionAuth(Auth):
         user = User.get(user_id)
         return user
 
-    def destroy_session(self, request=None) -> bool:
-        """ destroy the user session """
-        if request is None:
-            return False
-        session_id = self,session_cookie(request)
+    def destroy_session(self, request=None):
+        """Destroy the user session"""
+        session_id = self.session_cookie(request)
         user_id = self.user_id_for_session_id(session_id)
         if user_id is None:
             return False
+
         if session_id in self.user_id_by_session_id:
             del self.user_id_by_session_id[session_id]
         return True
