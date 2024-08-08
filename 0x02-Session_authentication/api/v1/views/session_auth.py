73a91b16a794c45b22460691ec36f56cf60d81da
mahakamal-e <mahakamal09@gmail.com>
2024-08-08 22:05:34 +0000

    edit

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -20,12 +20,12 @@ def login() -> str:
     users = User.search({'email': email})
     if users is None:
         return jsonify({"error": "no user found for this email"}), 404
-    
+
     user = users[0]
     is_valid_pwd = user.is_valid_password(password)
     if not is_valid_pwd:
         return jsonify({"error": "wrong password"}), 401
-    
+
     from api.v1.app import auth
     session_id = auth.create_session(user.id)
     response = jsonify(user.to_json())
