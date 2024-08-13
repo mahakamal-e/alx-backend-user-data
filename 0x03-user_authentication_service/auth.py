ae1f1662d189316531eb43d27330a0574f7cff87
mahakamal-e <mahakamal09@gmail.com>
2024-08-13 23:59:51 +0000

    edit

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -52,7 +52,7 @@ class Auth:
         try:
             user = self._db.find_user_by(session_id=session_id)
             return user
-        except NothresultFound:
+        except NotResultFound:
             return None
 
 
