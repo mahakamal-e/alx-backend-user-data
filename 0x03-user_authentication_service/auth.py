b97517240d99e7e1136fe2457e6b22b1f604582e
mahakamal-e <mahakamal09@gmail.com>
2024-08-13 19:11:22 +0000

    add

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,7 +2,7 @@
 """ Authentication Module """
 import bcrypt
 from db import DB
-from user impoert User
+from user import User
 from sqlalchemy.orm.exc import NoResultFound
 
 
@@ -13,14 +13,15 @@ class Auth:
     def __init__(self):
         self._db = DB()
     
-    def register_user(email: str, password: str) -> User:
+    def register_user(self, email: str, password: str) -> User:
         """Saved the user to the database after hashing password."""
         try:
             user = self._db.find_user_by(email=email)
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
-            hash_pass = _hash_password(password)
-            user = self._db.add_user(email=email, password=password)
+            hashed_password = _hash_password(password)
+            user = self._db.add_user(email=email,
+                                     hashed_password=hashed_password)
             return user
 
 
