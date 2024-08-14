fd345b7d4612a4dd03f60369b0aa1e57eb6b6dd8
mahakamal-e <mahakamal09@gmail.com>
2024-08-14 00:48:41 +0000

    add

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -68,9 +68,9 @@ class Auth:
         """Find the user corresponding to the email"""
         try:
             user = self._db.find_user_by(email=email)
-            token_uuid = _generate_uuid()
-            self.update_user(user.id, reset_token=token_uuid)
-            return token_uuid
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
         except NoResultFound:
             raise ValueError
 
